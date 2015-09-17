13684525fe82cfa2e2aa12ef5eb2ae612d4e544b
Wang Xiayang <wangxiayang.fdt@gmail.com>
2015-09-18 00:30:17 +0800

    add multi-thread support

--- webserv.c
+++ webserv.c
@@ -19,10 +19,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <pthread.h>
+
 #include "helper.h"
 #include "servreq.h"
 
 #define SERVER_PORT 8080
+
+void *serve_req(void *args) {
+	printf("\33[0;32mNew Connection..\33[m\n");
+
+	int conn = *(int *)args;
+	Service_Request(conn);
+
+	/*  Close connected socket and exit  */
+	if (close(conn) < 0)
+		Error_Quit("Error closing connection socket.");
+	printf("\33[0;34mConnection closed.\33[m\n");
+	free(args);
+
+	return NULL;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -52,19 +69,15 @@ int main(int argc, char *argv[]) {
 
     /*  Loop infinitely to accept and service connections  */
     while (1) {
-		int conn;
+		int *conn = (int *)malloc(sizeof(int));
 		/*  Wait for connection  */
-		if ((conn = accept(listener, NULL, NULL)) < 0)
+		if ((*conn = accept(listener, NULL, NULL)) < 0)
 		    Error_Quit("Error calling accept()");
-		printf("\33[0;32mNew Connection..\33[m\n");
 
-		Service_Request(conn);
-
-		/*  Close connected socket and exit  */
-		if (close(conn) < 0)
-			Error_Quit("Error closing connection socket.");
-		printf("\33[0;34mConnection closed.\33[m\n");
-    }
+		pthread_t t;
+		pthread_create(&t, NULL, serve_req, conn);
+		//pthread_join(t, NULL);
+	}
 
     return EXIT_FAILURE;    /*  We shouldn't get here  */
 }
